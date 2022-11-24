8843ca394536732d973017cfbc06eb28707be2cb
HectorAG-inco <hector.agonzalez@alumnos.udg.mx>
2022-11-23 22:52:20 -0600

    llenado de pdf y otros cambios del programa

--- reporte.php
+++ reporte.php
@@ -9,13 +9,26 @@ if(!isset($visitante)){
     header("location:log.php");
 }
 
-//generar la consulta para extraer los datos
+//generar la consulta para extraer los datos de la tabla mascotas
 $id = $_GET['idmascota'];
 $m = "SELECT * FROM mascotas WHERE idmascota = '$id'";
-$modificar = $conecta -> query($m); 
-$datos = $modificar->fetch_array();
+$seleccionar = $conecta -> query($m); 
+$datos = $seleccionar->fetch_array();
 
+//esta variable es para ponerle nombre al archivo
 $n_archivo = $datos['nombre'];
+//variable para buscar nombre del dueño de la mascota
+$n_cliente = $datos['id_dueno'];
+//generar la consulta para extraer los datos de la tabla clientes
+$c = "SELECT * FROM clientes WHERE idcliente = '$n_cliente'";
+$seleccionar_cliente = $conecta -> query($c); 
+$datos_cliente = $seleccionar_cliente->fetch_array();
+
+//generar la consulta para extraer los datos de la tabla vacunacion
+$v = "SELECT * FROM vacunacion WHERE id_mascota = '$id'";
+$seleccionar_vacuna = $conecta -> query($v); 
+$datos_vacuna = $seleccionar_vacuna->fetch_array();
+
 
 //se llena el bufer para guardar la pagina html en una variable y de esta manera poder convertirlo con DOMPDF
 ob_start();
@@ -29,7 +42,6 @@ ob_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reporte</title>
-    <link rel="stylesheet" href="pdf.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
@@ -46,17 +58,105 @@ ob_start();
     
     <h1 class="text-center mt-2 mb-2 fw-bold">Historial Médico</h1>
 
-    <p>
-        <h4 style="font-family: 'Hind', sans-serif;" class="ms-3 me-3 fw-semibold">Nombre: <small class="text-muted"><?php echo $datos['nombre']?></small> </h4>
-        <h4 style="font-family: 'Hind', sans-serif;" class="ms-3 me-3 fw-semibold">Raza: <small class="text-muted"><?php echo $datos['raza']?></small> </h4>
-        <h4 style="font-family: 'Hind', sans-serif;" class="ms-3 me-3 fw-semibold">Color: <small class="text-muted"><?php echo $datos['color']?></small> </h4>
-        <h4 style="font-family: 'Hind', sans-serif;" class="ms-3 me-3 fw-semibold">Género: <small class="text-muted"><?php echo $datos['raza']?></small> </h4>
-        <h4 style="font-family: 'Hind', sans-serif;" class="ms-3 me-3 fw-semibold">Especie: <small class="text-muted"><?php echo $datos['especie']?></small> </h4>
-        <h4 style="font-family: 'Hind', sans-serif;" class="ms-3 me-3 fw-semibold">Peso: <small class="text-muted"><?php echo $datos['peso']?> Kg</small> </h4>
-        <h4 style="font-family: 'Hind', sans-serif;" class="ms-3 me-3 fw-semibold">Edad: <small class="text-muted"><?php echo $datos['edad']?> Años</small> </h4>
-        <h4 style="font-family: 'Hind', sans-serif;" class="ms-3 me-3 fw-semibold">Fecha de nacimiento: <small class="text-muted"><?php echo $datos['fecha']?></small> </h4>
-    </p>
-    <!--script de bootstrap-->
+    <div class="container text-center">
+        <div class="row row-cols-auto">
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Nombre:</h3>
+            </div>
+            <div class="col">
+                <small class="text-muted"><?php echo $datos['nombre']?></small>
+            </div>
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Raza:</h3>
+            </div>
+            <div class="col">
+            <small class="text-muted"><?php echo $datos['raza']?></small> 
+            </div>
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Nombre de dueño:</h3>
+            </div>
+            <div class="col">
+            <small class="text-muted"><?php echo $datos_cliente['nombre_cliente']?> <?php echo $datos_cliente['apellido1']?> <?php echo $datos_cliente['apellido2']?></small> 
+            </div>
+        </div>
+
+        <div class="row row-cols-auto">
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Color:</h3>
+            </div>
+            <div class="col">
+                <small class="text-muted"><?php echo $datos['color']?></small>
+            </div>
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Género:</h3>
+            </div>
+            <div class="col">
+            <small class="text-muted"><?php echo $datos['genero']?></small> 
+            </div>
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Especie:</h3>
+            </div>
+            <div class="col">
+            <small class="text-muted"><?php echo $datos['especie']?></small> 
+            </div>
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Fecha de nacimiento:</h3>
+            </div>
+            <div class="col">
+            <small class="text-muted"><?php echo $datos['fecha']?></small> 
+            </div>
+        </div>
+
+        <div class="row row-cols-auto">
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Edad:</h3>
+            </div>
+            <div class="col">
+                <small class="text-muted"><?php echo $datos['edad']?></small>
+            </div>
+            <div class="col">
+                <h3 class="ms-3 me-3 fw-semibold">Peso:</h3>
+            </div>
+            <div class="col">
+            <small class="text-muted"><?php echo $datos['peso']?></small> 
+            </div>
+        </div>
+    </div>
+
+    <!--Inicio de tabla vacunas-->
+    <div class="container">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <h4 class="text-center mt-3">Vacunas vigentes</h4>
+            <div class="table-responsive table-hover" id="TablaConsulta">
+                <table class="table">
+                    <thead class="text-muted">
+                        <th class="text-center">Moquillo</th>
+                        <th class="text-center">Parvovirus</th>
+                        <th class="text-center">Polivalente</th>
+                        <th class="text-center">Bordetella</th>
+                        <th class="text-center">Leptospira</th>
+                        <th class="text-center">Triple felina</th>
+                        <th class="text-center">Leucemia</th>
+                        <th class="text-center">Rabia</th>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td class="text-center"><?php echo $datos_vacuna['moquillo'];?></td>
+                            <td class="text-center"><?php echo $datos_vacuna['parvovirus'];?></td>
+                            <td class="text-center"><?php echo $datos_vacuna['polivalente'];?></td>
+                            <td class="text-center"><?php echo $datos_vacuna['bordetella'];?></td>
+                            <td class="text-center"><?php echo $datos_vacuna['leptospira'];?></td>
+                            <td class="text-center"><?php echo $datos_vacuna['triple_felina'];?></td>
+                            <td class="text-center"><?php echo $datos_vacuna['leucemia'];?></td>
+                            <td class="text-center"><?php echo $datos_vacuna['Rabia'];?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>     
+    </div>
+    <!--fin de tabla vacunas-->
+        <!--script de bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     
 </body>
